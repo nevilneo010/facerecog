bcf76702d762593b96c99abf1b714518bf3317f9
nevilneo <nevilneo010@gmail.com>
2019-07-30 02:11:00 +0530

    defined as function

--- face_detect_cv3.py
+++ face_detect_cv3.py
@@ -1,8 +1,8 @@
 import cv2
 import sys
-from firebase import firebase 
+#from firebase import firebase 
 import time;
-firebase = firebase.FirebaseApplication('https://iris-687c2.firebaseio.com/', None)   
+#firebase = firebase.FirebaseApplication('https://iris-687c2.firebaseio.com/', None)   
 # Get user supplied values
 #imagePath = sys.argv[1]
 cascPath = "haarcascade_frontalface_default.xml"
@@ -11,41 +11,49 @@ cascPath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascPath)
 
 video_capture = cv2.VideoCapture(0)
-z=0;
-while True:
-    # Capture frame-by-frame
-    ret, frame = video_capture.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
-        #flags=cv2.cv.CV_HAAR_SCALE_IMAGE
-    )
+z=0
+def main():
+    global cascPath,faceCascade,video_capture,z
+    while True:
+        # Capture frame-by-frame
+        ret, frame = video_capture.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            #flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+        )
 
-    # Draw a rectangle around the faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        print("face detected")
-        if(z==0):
-            result = firebase.put('/','screen',1)  
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            print("face detected")
+            if(z==0):
+                #print(z)
+                return 1
+    #           result = firebase.put('/','screen',1)  
 
-        z=100
-    time.sleep(.1)
-    if(z>1):
-        z=z-1
-    print(z)
-    if(z==1):
-        result = firebase.put('/','screen',0)  
-        z=0
+            z=100
+        time.sleep(.1)
+        if(z>1):
+            z=z-1
+        print(z)
+        if(z==1):
+            z=0
+            return 0
+    #      result = firebase.put('/','screen',0)  
+            
 
-    # Display the resulting frame
-    cv2.imshow('Video', frame)
+        # Display the resulting frame
+        cv2.imshow('Video', frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-# When everything is done, release the capture
-video_capture.release()
-cv2.destroyAllWindows()
+    # When everything is done, release the capture
+    video_capture.release()
+    cv2.destroyAllWindows()
+if __name__  == '__main__':
+    main()
